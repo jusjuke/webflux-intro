b5f11afbf8d81a71745eb8a6530a7779ba28c575
Jusik Yoon <jusjuke@gmail.com>
2023-07-03 20:27:36 +0900

    ..

--- src/main/java/kr/co/webmill/webfluxintro/controller/ReactiveMathController.java
+++ src/main/java/kr/co/webmill/webfluxintro/controller/ReactiveMathController.java
@@ -2,13 +2,17 @@ package kr.co.webmill.webfluxintro.controller;
 
 import kr.co.webmill.webfluxintro.dto.Response;
 import kr.co.webmill.webfluxintro.service.ReactiveMathService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
@@ -24,7 +28,11 @@ public class ReactiveMathController {
         return reactiveMathService.findSquare(input);
     }
     @GetMapping("table/{input}")
-    public Flux<Response> multiplicationTable(@PathVariable int input){
+    public Flux<Response> multiplicationTable(@PathVariable int input, ServerWebExchange serverWebExchange){
+        return reactiveMathService.multiplicationTable(input);
+    }
+    @GetMapping(value = "table/{input}/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Response> multiplicationTableStream(@PathVariable int input){
         return reactiveMathService.multiplicationTable(input);
     }
 }
--- src/main/java/kr/co/webmill/webfluxintro/service/ReactiveMathService.java
+++ src/main/java/kr/co/webmill/webfluxintro/service/ReactiveMathService.java
@@ -2,8 +2,12 @@ package kr.co.webmill.webfluxintro.service;
 
 import kr.co.webmill.webfluxintro.dto.Response;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.Disposable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @Service
 public class ReactiveMathService {
@@ -12,11 +16,39 @@ public class ReactiveMathService {
                 .map(Response::new);
     }
 
-    public Flux<Response> multiplicationTable(int input) {
+/**
+    public Flux<Response>   multiplicationTable(int input) {
         return Flux.range(1, 10)
                 .doOnNext(i -> SleepUtil.sleepSeconds(1))
                 .doOnNext(i -> System.out.println("reactive-math-service is processing: " + i))
                 .map(i -> new Response(i * input));
 
     }
+**/
+
+    public Flux<Response> multiplicationTable(int input) {
+        Flux<Long> range = Flux.range(1, 10)
+                .delayElements(Duration.ofSeconds(1))
+                .map(Long::valueOf);
+/**
+        Mono<Void> cancellationSignal = Mono.create(sink -> {
+            //Disposable disposable = serverWebExchange.getRequest().getAborted()
+            Disposable disposable = serverWebExchange.getRequest().
+                    .doOnTerminate(() -> sink.success())
+                    .subscribe();
+
+            sink.onCancel(disposable::dispose);
+        });
+**/
+
+/**
+        Mono<Long> cancellationSignal = Mono.delay(Duration.ofSeconds(5))
+                .doOnSubscribe(subscription -> System.out.println("Cancellation signal started."));
+**/
+        return range
+                .doOnNext(i -> System.out.println("reactive-math-service is processing: " + i))
+     //           .takeUntilOther(cancellationSignal)
+                .map(i -> new Response((int) (i * input)));
+    }
+
 }
